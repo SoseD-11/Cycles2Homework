a83a26999b964941ec00693313e70b33d03fd593
Sosed_11 <kost1991ya@mail.ru>
2022-10-10 11:32:54 +0500

    Cycles2 finish

--- src/Main.java
+++ src/Main.java
@@ -24,60 +24,58 @@ public class Main {
         System.out.println();
         //Задание-3
         int populationY = 12_000_000;
-        int fertilityYear = populationY / 1000 * 17;
-        int mortalityYear = populationY / 1000 * 8;
-        int increasePerYear = fertilityYear - mortalityYear;
+        float fertilityYear = (float)17/1000;
+        float mortalityYear = (float)8/1000;
+        float increasePerYear = fertilityYear - mortalityYear;
         int year = 0;
         while(year < 10){
-        populationY = populationY + increasePerYear;
+        populationY = populationY + Math.round(populationY * increasePerYear);
         year++;
         System.out.println("Год " + year + " численность населения составляет " + populationY);
         }
         System.out.println("Домашнее задание-2");
         //Задание-1
         int firstAccumulation = 15_000;
-        int m = 0;
+        int monthS = 0;
         while(firstAccumulation < 12_000_000) {
-            firstAccumulation = firstAccumulation + firstAccumulation * 7 / 100;
-            m++;
-            System.out.println("Месяц " + m + " сумма накоплений равна " + firstAccumulation + " рублей" );
+            firstAccumulation += firstAccumulation * 7 / 100;
+            monthS++;
+            System.out.println("Месяц " + monthS + " сумма накоплений равна " + firstAccumulation + " рублей" );
         }
         System.out.println("Задание-2");
         int first = 15_000;
-        int mo = 0;
+        int monthZ = 0;
         while(first < 12_000_000){
             first = first + first * 7/100;
-            mo++;
-            if(mo % 6 == 0){
-                System.out.println("Месяц " + mo + " сумма накоплений равна " + first + " рублей");
+            monthZ++;
+            if(monthZ % 6 == 0){
+                System.out.println("Месяц " + monthZ + " сумма накоплений равна " + first + " рублей");
             }
         }
         //Задание-3
         int f = 15_000;
-        int mon = 0;
-        while(mon < 12 * 9) {
+        int monthC = 0;
+        while(monthC < 12 * 9) {
         f = f +f * 7/100;
-        mon++;
-        if(mon % 6 == 0){
-            System.out.println("Месяц " + mon + " сумма накоплений равна " + f + " рублей");
+        monthC++;
+        if(monthC % 6 == 0){
+            System.out.println("Месяц " + monthC + " сумма накоплений равна " + f + " рублей");
         }
         }
         //Задание-4
         int friday = 1;
         while (friday <= 31){
-            friday = friday + 7;
-            if(friday<=31) {
-                System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет");
-            }
+            System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет");
+         friday += 7;
         }
         // Домашнее задание-3.
 
         System.out.println("   ДЗ 3.");
         // Задание - 1.
         System.out.println("   Задание - 1.");
-        int nowadays = 2022;
-        int year1 = nowadays - 200;
-        int future = nowadays + 100;
+        int nowADays = 2022;
+        int year1 = nowADays - 200;
+        int future = nowADays + 100;
         for (int u = year1; u <= future; u++){
             if (i % 79 ==0){
                 System.out.println(u);
